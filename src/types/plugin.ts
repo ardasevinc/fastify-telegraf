e6d216f17efcfae314e8ac4c5b7301dd5a5bc0d7
Arda Sevinç <arda@ardasevinc.com>
2024-07-04 22:21:55 +0300

    feat: make some fields optional

--- src/types/plugin.ts
+++ src/types/plugin.ts
@@ -1,12 +1,12 @@
 import { Type, Static } from '@fastify/type-provider-typebox';
 
 const TFastifyTelegrafOptions = Type.Object({
-  decoratorBotName: Type.String({ default: 'telegramBot' }),
-  waitForHealthPolling: Type.Integer(),
-  baseUrl: Type.String(),
-  webhookSecret: Type.String(),
+  decoratorBotName: Type.Optional(Type.String({ default: 'telegramBot' })),
+  waitForHealthPolling: Type.Optional(Type.Integer()),
+  baseUrl: Type.Optional(Type.String()),
+  webhookSecret: Type.Optional(Type.String()),
   botToken: Type.Readonly(Type.String()),
-  onUnhandledError: Type.Function([Type.Any()], Type.Void()),
+  onUnhandledError: Type.Optional(Type.Function([Type.Any()], Type.Void())),
 });
 
 type FastifyTelegrafOptions = Static<typeof TFastifyTelegrafOptions>;
